b548ab89229a74ebff48a5b17db0339d85b2cc2c
kmseonhwa <seonhwakei@gmail.com>
2022-07-08 15:16:46 +0900

    Add: color palette

---
+++ src/lib/styles/palette.ts
@@ -0,0 +1,17 @@
+const palette = {
+    /* lichenBlue */
+    lichenBlue0: '#87C5FF',
+    lichenBlue1: '#6594BF',
+    lichenBlue2: '#5E89B2',
+    lichenBlue3: '#517699',
+    lichenBlue4: '#3D5973',
+
+    /* gray */
+    gray0: '#E6E6E6',
+    gray1: '#DADADA',
+    gray2: '#BFBFBF',
+    gray3: '#999999',
+    gray4: '#595959',
+};
+
+export default palette;
